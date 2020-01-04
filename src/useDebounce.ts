46dc1f488dd33dd9c713c924b5a0d56e71b79b77
宋冰冰 <songbingbing@songbingbingdeMacBook-Pro.local>
2020-01-04 11:41:22 +0800

    fix:修改useDebounce和useThrottle的传参问题

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,6 +1,15 @@
 function useDebounce(fn: Function, delay: number, immediate: boolean) {
-  let time = null;
-  return function() {};
+  let time: number | undefined = undefined;
+  return function(...args) {
+    let that = this;
+    if (immediate && !time) {
+      fn.apply(that, args);
+    }
+    clearTimeout(time);
+    time = setTimeout(() => {
+      fn.apply(that, args);
+    }, delay);
+  };
 }
 
 export default useDebounce;
