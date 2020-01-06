1da972bd67794974175d92fa2413ad44e36593ba
宋冰冰 <songbingbing@songbingbingdeMacBook-Pro.local>
2020-01-06 16:39:01 +0800

    fix: useImmutableHelper.ts 修改类型注解

--- src/useImmutableHelper.ts
+++ src/useImmutableHelper.ts
@@ -1,16 +1,16 @@
-import { useCallback } from 'react';
+import { useCallback, MutableRefObject } from 'react';
 import update from 'immutability-helper';
 
 import useSetState from './useSetState';
 
-function useImmutableHelper<T = any>(initialState: T): [T, Function] {
-  const [state, setState] = useSetState(initialState);
-  return [
-    state as any,
-    useCallback(params => {
-      setState(state => update(state, params));
-    }, []),
-  ];
+function useImmutableHelper<T = any>(
+  initialState: T
+): [MutableRefObject<T | undefined>, Function] {
+  const [state, setState] = useSetState<T>(initialState);
+  const dispatch = useCallback(params => {
+    setState(state => update(state, params));
+  }, []);
+  return [state, dispatch];
 }
 
 export default useImmutableHelper;
