f70b95424f8ccd5d7423847c0b7bf4c01937892b
宋冰冰 <songbingbing@songbingbingdeMacBook-Pro.local>
2020-01-09 15:04:11 +0800

    fix:修改useForceUpdate.ts的返回

--- src/useForceUpdate.ts
+++ src/useForceUpdate.ts
@@ -3,10 +3,13 @@ import { useCallback, useState } from 'react';
 const increment = (value: number): number => ++value % 10000;
 
 function useForceUpdate() {
-  const [, setState] = useState(0);
-  return useCallback(() => {
-    setState(increment);
-  }, []);
+  const [flag, setState] = useState(0);
+  return [
+    flag,
+    useCallback(() => {
+      setState(increment);
+    }, []),
+  ];
 }
 
 export default useForceUpdate;
